d6d1f0c51de85c3d282578639c28247b9b19c85b
Artlfmj <73163159+Artlfmj@users.noreply.github.com>
2021-03-13 14:00:28 +0100

    add de commandes pas fonctionnels. Oui ya pas dinteret

--- commands/fortnite/shop.js
+++ commands/fortnite/shop.js
@@ -10,13 +10,13 @@ module.exports = {
     cooldown: 2,
     usage: "s!shop",
     run: async (client, message, args, user, text, prefix) => {
-        let { body } = await superagent.get("https://fortool.fr/cm/api/v1/shop")
+        let { body } = await superagent.get("https://fortool.fr/cm/api/v1/shop?lang=fr")
         const Shop = new Discord.MessageEmbed()
 	    .setColor('#0099ff')
 	    .setTitle('Shop Fortnite')
 	    .setAuthor('Shop Bot par Artlfmj#0310', 'https://imgur.com/a/NyPT4RO', 'https://www.youtube.com/channel/UCLee-XQyphWxB7mQ_beGIyg')
-	    .setDescription(`Shop Fortnite du jour: https://api.nitestats.com/v1/shop/image`)
-        .setImage(' https://api.nitestats.com/v1/shop/image')
+	    .setDescription(`Shop Fortnite du jour: `)
+        .setImage(body.images.default)
         .setTimestamp()
 	    .setFooter('Copyright Intermarket 2021', 'https://cdn.discordapp.com/attachments/745266722692530259/768033163880300574/logo.png');
 
---
+++ handlers/server.js
@@ -0,0 +1,6 @@
+const app = require('express')()
+
+module.exports=async (bot) => {
+
+    app.listen()
+}
