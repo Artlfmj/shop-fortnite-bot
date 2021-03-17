79ab953d32db2c48fd45886f168faf5d93eae956
Artlfmj <73163159+Artlfmj@users.noreply.github.com>
2021-03-17 15:09:57 +0100

    Update blagues.js

--- commands/fun/blagues.js
+++ commands/fun/blagues.js
@@ -3,7 +3,8 @@ const nodefetch = require('node-fetch')
 const Discord = require('discord.js')
 const superagent = require('superagent');
 const config = require('./blagues.json')
-const fetch = import('node-fetch')
+const request = require('request');
+const tokenBlaguesAPI = config.blagues;
 
 module.exports = {
     name: "blague",
@@ -15,16 +16,45 @@ module.exports = {
     usage: "modes",
     run: async (client, message, args, user, text, prefix) => {
     
-        fetch('https://www.blagues-api.fr/api/random', {
-            headers: {
-                'Authorization': `Bearer ${config.blagues}`
-            }
-        })
-        let Blague = new Discord.MessageEmbed()
-        .setTitle("Blague")
-        .setDescription(`${response.question} \n || ${response.answer} ||`)
-        
-        message.channel.send(Blague)
+        let url = 'https://www.blagues-api.fr/api/' + (args[0] ? ("type/" + args[0] + '/random') : 'random');
+            request(url, {
+                    headers: {
+                        'Authorization': `Bearer ` + tokenBlaguesAPI
+                    }
+                },
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        const jokeBody = JSON.parse(body);
+                        let typeBlague = "";
+                        switch (jokeBody.type) {
+                            case "global":
+                                typeBlague = "tout public";
+                                break;
+                            case "dev":
+                                typeBlague = "de développeur";
+                                break;
+                            case "dark":
+                                typeBlague = "humour noir";
+                                break;
+                            case "limit":
+                                typeBlague = "limite limite";
+                                break;
+                            case "beauf":
+                                typeBlague = "de beauf";
+                                break;
+                            case "blondes":
+                                typeBlague = "sur les blondes";
+                                break;
+                        }
+                        const embed = new Discord.MessageEmbed()
+                            .setTitle(jokeBody.joke)
+                            .setColor(0x00bfff)
+                            .setDescription("||" + jokeBody.answer + "||")
+                            .setFooter('Blague ' + typeBlague, message.guild.iconURL())
+                            .setTimestamp();
+                        message.channel.send(embed);
+                    }
+                });
 
     }
 };
