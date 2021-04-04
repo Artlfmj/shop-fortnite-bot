f1f5e2b3832d0438ec0a42f576f9933a0e7a5d23
Artlfmj <73163159+Artlfmj@users.noreply.github.com>
2021-04-04 18:07:44 +0200

    Correction setStatus

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 //here the event starts
 module.exports = client => {
     console.log(`Discord Bot ${client.user.tag} is online!`); //log when ready aka the bot usable
-    client.user.setActivity(`BETA SHOP | VERSION 1.2.1 en beta`, { type: "WATCHING"}) //first parameter, is the status, second is an object with type which can be: "PLAYING", "WATCHING", "LISTENING", "STREAMING" (where you need to add a , and then url: "https://twitch.tv/#")
+    client.user.setActivity(`SHOP s!help | VERSION 1.2.1 `, { type: "WATCHING"}) //first parameter, is the status, second is an object with type which can be: "PLAYING", "WATCHING", "LISTENING", "STREAMING" (where you need to add a , and then url: "https://twitch.tv/#")
 }
