019081f7b53505f6a6faf79973b1ce1541f678e7
Artlfmj <73163159+Artlfmj@users.noreply.github.com>
2021-04-04 19:06:49 +0200

    Ajouts events

---
+++ events/guildCreate.js
@@ -0,0 +1,11 @@
+//here the event starts
+const Discord = require('discord.js')
+
+module.exports = client => {
+    console.log(guild)
+    const embed1 = new Discord.MessageEmbed()
+    .setTitle(`Merci à ${member.username}`)
+    
+    .setDescription(`Bienvenue ${member} dans TECHNCODE. Tu peux consulter les regles dans <#822747635965755433>. Accepte bien le reglement afin de profiter de tous nos services`)
+    message.guild.channels.cache.get('828297901956399134').send(embed1)
+}
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,15 +5,15 @@ module.exports = async (client, member) => {
     //logs when a member joins, make sure to have GuildMemberIntents active in discord.com/developers
     console.log(member + "joined in: " + member.guild.name)
     if(member.guild.id === "822747635517227009"){
-        const embed1 = new Discord.MessageEmbed()
-        .setTitle(`Bienvenue à ${member.username}`)
-        .setImage(member.displayAvatarURL({
-            dynamic: true,
-            size: 512,
-            format: "png",
-        }))
-        .setDescription(`Bienvenue ${member} dans TECHNCODE. Tu peux consulter les regles`)
-        message.guild.channels.cache.get('828297901956399134').send(embed1)
+        //const embed1 = new Discord.MessageEmbed()
+        //.setTitle(`Bienvenue à ${member.username}`)
+        //.setImage(member.displayAvatarURL({
+        //    dynamic: true,
+        //    size: 512,
+        //    format: "png",
+        //}))
+        //.setDescription(`Bienvenue ${member} dans TECHNCODE. Tu peux consulter les regles dans <#822747635965755433>. Accepte bien le reglement afin de profiter de tous nos services`)
+        //message.guild.channels.cache.get('828297901956399134').send(embed1)
     }
 };
 
