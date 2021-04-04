21e69eb4c88b16c2f6ff15aa35549b7167497223
Artlfmj <73163159+Artlfmj@users.noreply.github.com>
2021-04-04 18:07:57 +0200

    ajout envoi de messages

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,33 +1,19 @@
 //here the event starts
 const Discord = require('discord.js')
-const canvas = require('canvas')
-const Canvas = require('discord-canvas')
+
 module.exports = async (client, member) => {
     //logs when a member joins, make sure to have GuildMemberIntents active in discord.com/developers
     console.log(member + "joined in: " + member.guild.name)
     if(member.guild.id === "822747635517227009"){
-        let avatar = member.displayAvatarURL({
+        const embed1 = new Discord.MessageEmbed()
+        .setTitle(`Bienvenue à ${member.username}`)
+        .setImage(member.displayAvatarURL({
             dynamic: true,
             size: 512,
             format: "png",
-        })
-        const image = new Canvas.Welcome()
-        .setUsername(member.username)
-        .setDiscriminator(member.tag)
-        .setMemberCount(member.guild.memberCount)
-        .setGuildName(member.guild.name)
-        .setAvatar(avatar)
-        .setColor("border", "#8015EA")
-        .setColor("username-box", "#8015EA")
-        .setColor("discriminator-box", "#8015EA")
-        .setColor("message-box", "#8015EA")
-        .setColor("title", "#8015EA")
-        .setColor("avatar", "#8015EA")
-        .setBackground("https://site.com/background.jpg")
-        .toAttachment();
-    
-        const attachment = new Discord.MessageAttachment(image.toBuffer(), "welcome-image.png");
-        message.channel.send(attachment);
+        }))
+        .setDescription(`Bienvenue ${member} dans TECHNCODE. Tu peux consulter les regles`)
+        message.guild.channels.cache.get('828297901956399134').send(embed1)
     }
 };
 
