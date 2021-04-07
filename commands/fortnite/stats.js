46c12643f6251476b5aaae94e149063b76d2492e
Artlfmj <73163159+Artlfmj@users.noreply.github.com>
2021-04-07 14:21:03 +0200

    Nouvelles commandes

--- commands/fortnite/stats.js
+++ commands/fortnite/stats.js
@@ -1,31 +1,41 @@
 const Discord = require('discord.js')
 const superagent = require('superagent');
+const fs = require('fs');
+const fetch = require('node-fetch');
 
 module.exports = {
     name: "stats",
-    aliases: ["aliase"],
+    aliases: ["stastistiques"],
     description: "Envoie stats",
     category: "fortnite",
     guildOnly: true,
     cooldown: 2,
     usage: "stats",
     run: async (client, message, args, user, text, prefix) => {
-        let { body } = await superagent.get('https://fortnite-api.com/v1/stats/br/v2');
-        message.delete();
+        message.delete()
+        const Wait = new Discord.MessageEmbed()
+        .setTitle("CHARGEMENT DES STATISTIQUES")
+        .setTimestamp()
+        const msg = await message.channel.send(Wait)
+        message.channel.startTyping()
+        let { body } = await superagent.get(`https://fortnite-api.com/v1/stats/br/v2?name=${args[0]}&image=all`);
         let Stats = new Discord.MessageEmbed()
         .setTitle("Statistiques du joueur")
         .setColor('#0099ff')
-        .setDescription('Voici les statistiques du joueur concerné')
+
+        .setDescription('Voici les statistiques globales du joueur concerné')
         .addFields(
-        { name: 'Regular field title', value: 'Some value here' },
-        { name: '​', value: '​' },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
-        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: `Kills globaux`, value: body.data.stats.all.overall.kills, inline: true },
+        { name: 'Top 1​', value: body.data.stats.all.overall.wins, inline: true },
+        { name: "Parties", value: body.data.stats.all.overall.matches , inline: true },
+        { name: 'Ratio de kill', value: body.data.stats.all.overall.kd + "%", inline: true },
         )
+        .addField("Pourcentage de victoires", body.data.stats.all.overall.winRate, true)
         .setTimestamp()
-        .setFooter('Some footer text here', 'https://i.imgur.com/hnrWVnt.png');
-        
-        message.channel.send(Stats)
+        .setImage(body.data.image)
+        message.channel.stopTyping()
+        msg.edit(Stats)
+       
 
     }
 };
